9fe966ba672ebd286a6a259284d03220ad968328
sienna <20200803@sungshin.ac.kr>
2022-05-07 14:03:30 +0900

    상품 조회 - 오류

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        //home.html이 호출됨
+        //home.html이 호출됨 mapping
         return "home";
     }
 
